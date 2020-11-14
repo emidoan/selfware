5fe85995f300bc476312efb6be9cdb4208908645
Hanh Binh Nguyen <mmnguy10@uci.edu>
2020-11-14 15:56:59 -0800

    adding meditation

--- src/App.js
+++ src/App.js
@@ -6,18 +6,26 @@ import '../node_modules/react-vis/dist/style.css';
 
 // Installed dependency imports
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import {
+  Link
+} from "react-router-dom";
 
 // Website imports for classes you made
 import { UserList } from "app/views";
+import { Meditation } from "app/views";
+
 
 function App() {
   return (
     <div className="app fill-view">
+      <h1>SelfWare</h1>
       <Router>
+            <Link to="/">Home</Link>
+            <Link to="/Meditation">Meditation</Link>
         <Switch>
           <Route 
-            exact path={"/"}
-            component={UserList}
+            exact path={"/meditation"}
+            component={Meditation}
           />
         </Switch>
       </Router>
---
+++ src/app/views/meditation/meditation.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import "./meditation.scss";
+
+import axios from "axios"
+
+import { UserCard } from "app/components";
+
+function Meditation() {
+  return (
+    <div className="user-list">
+      <h1>Meditation Page</h1>
+    </div>
+  );
+}
+
+export default Meditation;
