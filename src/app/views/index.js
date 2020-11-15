6c8f41a45f1e8d5ed7b6d5f318d014832138372a
Emily Doan <edoan2@uci.edu>
2020-11-15 03:00:21 -0800

    New menu to redirect to journal entry

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -1,3 +1,4 @@
 export { default as UserList } from "./user-list/userList";
 export { default as Meditation } from "./meditation/meditation";
 export { default as Journal } from "./journal/journal";
+export { default as JournalEntry } from "./journal/journal-entry/journalEntry";
