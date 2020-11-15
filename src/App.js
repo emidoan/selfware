185c4cddd049be2ba6eb851e70b39f1448340c6c
Annahi Paez <aspaez@uci.edu>
2020-11-15 10:19:32 -0800

    DOne

--- src/App.js
+++ src/App.js
@@ -6,62 +6,42 @@ import '../node_modules/react-vis/dist/style.css';
 
 // Installed dependency imports
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-<<<<<<< HEAD
-import {
-  Link
-} from "react-router-dom";
-=======
 import { Link } from "react-router-dom";
->>>>>>> 67262680559e664fb91a5c6ec88b5f3c532de0fa
 
 // Website imports for classes you made
 import { UserList } from "app/views";
 import { Meditation } from "app/views";
-<<<<<<< HEAD
-
-=======
 import {Journal} from "app/views";
 import {JournalEntry} from "app/views";
 import {PastJournal} from "app/views";
 
 import { Resources } from "app/views";
->>>>>>> 67262680559e664fb91a5c6ec88b5f3c532de0fa
 
 function App() {
   return (
     <div className="app fill-view">
       <h1>SelfWare</h1>
+      <h2>Medidate freely.</h2>
+      
       <Router>
-<<<<<<< HEAD
-            <Link to="/">Home</Link>
-            <Link to="/Meditation">Meditation</Link>
-        <Switch>
-          <Route 
-            exact path={"/meditation"}
-            component={Meditation}
-          />
-=======
-
         <Link to ="/">Home</Link>
         <Link to ="/meditation">Meditation</Link>
         <Link to ="/journal">Journal</Link>
-<<<<<<< HEAD
         <Link to ="/pastjournal">Load Past Journals</Link>
-=======
         <Link to ="/resources">Resources</Link>
->>>>>>> resources
-
+            {/* <Link to="/">Home</Link>
+            <Link to="/Meditation">Meditation</Link> */}
         <Switch>
+          <Route
+            exact path={"/meditation"}
+            component={Meditation}
+          />
+
           <Route exact path={'/'}component={Home}/>
           <Route exact path={"/meditation"}component={Meditation}/>
-<<<<<<< HEAD
           <Route path={"/journal"}component={Journal}></Route>
           <Route path={"/pastjournal"}component={PastJournal}/>
-=======
-          <Route exact path={"/journal"}component={Journal}/>
           <Route exact path={"/resources"}component={Resources}/>
->>>>>>> resources
->>>>>>> 67262680559e664fb91a5c6ec88b5f3c532de0fa
         </Switch>
       </Router>
     </div>
@@ -71,8 +51,6 @@ function App() {
 function Home() {
   return (
     <div>
-    <h1>SelfWare</h1>
-    <h2>Medidate freely.</h2>
   </div>
   );
 }
